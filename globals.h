db382c53d90cbe1c705473f1a2e457e25205546a
Pol Vilà Saló <polvilasalo@gmail.com>
2016-11-12 00:34:53 +0100

    Add globals with some helpers (+1 squashed commits)
    Squashed commits:
    
    [d41d9ed] Add vcxproj for globals

---
+++ globals.h
@@ -0,0 +1,13 @@
+﻿#ifndef __Globals__
+#define __Globals__
+#include <vector>
+
+using namespace std;
+
+string ToLowerCase(string sentence);
+void ToBold(string& sentence);
+vector<string> Split(string& command, const string& delimiter);
+
+
+#endif //__Globals__
+
